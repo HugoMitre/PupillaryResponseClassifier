af2b935f05ad934b020cf115cb595830a37b9268
unknown <jorge.rodriguez@cimat.mx>
2021-11-22 21:14:28 -0700

    Se agrega recall, f1-score a los clasificadores y la variable BLPS

--- Classifiers/Codes/LDA.py
+++ Classifiers/Codes/LDA.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 import itertools
 
 # Se carga archivo csv con las métricas calculadas para cada prueba.
-datos = pd.read_csv('D:/VariablesVisual.csv')
+datos = pd.read_csv('C:/Users/josan/OneDrive/Documentos/Tesis/Memoria de trabajo/Analisis/analisis-tareas-de-memoria-de-trabajo/VariablesVisualActualizado.csv')
 datos=datos[['nivel','blps','mpdc','apcps','pd1','entropy','TTP','PST']]
 datos=datos.astype(float)
 
@@ -27,11 +27,23 @@ seis = []
 macro = []
 weighted = []
 subconjunto=[]
+recallUno=[]
+recallTres=[]
+recallSeis=[]
+recallMacro=[]
+recallWeighted=[]
+f1Uno=[]
+f1Tres=[]
+f1Seis=[]
+f1Macro=[]
+f1Weighted=[]
 count = 1
 for features in all_combinations:
     features = list(features)
     X=datos[features]
-
+    #print('Cantidad de pruebas por nivel:')
+    #print(datos['nivel'].value_counts())
+    #print(X.columns)
     
     # Se separan los datos para entrenamiento (80%) y evaluación (20%).
     X_train, X_test, y_train, y_test = train_test_split(X, y,
@@ -52,6 +64,8 @@ for features in all_combinations:
 
     # Matriz de confusión y reporte de estadísticas.
     y_pred = clf.predict(X_test)
+    #print(confusion_matrix(y_test,y_pred))
+    #print(classification_report(y_test,y_pred))
     reporte = classification_report(y_test,y_pred)
      
     uno.append(reporte[74:78])
@@ -60,7 +74,18 @@ for features in all_combinations:
     macro.append(reporte[291:295])
     weighted.append(reporte[345:349])
     subconjunto.append('/'.join(features))
+    recallUno.append(reporte[84:88])
+    recallTres.append(reporte[138:142])
+    recallSeis.append(reporte[192:196])
+    recallMacro.append(reporte[301:305])
+    recallWeighted.append(reporte[355:359])
+    f1Uno.append(reporte[94:98])
+    f1Tres.append(reporte[148:152])
+    f1Seis.append(reporte[202:206])
+    f1Macro.append(reporte[311:315])
+    f1Weighted.append(reporte[365:369])
     print(count)
     count += 1
-reporte = pd.DataFrame({'subconjunto':subconjunto,'uno':uno,'tres':tres,'seis':seis,'macro':macro,'weighted':weighted})
-reporte.to_excel('LDA.xlsx')
+    #print("********************")
+reporte = pd.DataFrame({'Subset':subconjunto,'Precision:L':uno,'Precision:M':tres,'Precision:H':seis,'Precision:Macro':macro,'Precision:Weighted':weighted,'Recall:L':recallUno,'Recall:M':recallTres,'Recall:H':recallSeis,'Recall:Macro':recallMacro,'Recall:Weighted':recallWeighted,'F1:L':f1Uno,'F1:M':f1Tres,'F1:H':f1Seis,'F1:Macro':f1Macro,'F1:Weighted':f1Weighted})
+reporte.to_excel('LDA2.xlsx')
--- Classifiers/Codes/SVM_Linear.py
+++ Classifiers/Codes/SVM_Linear.py
@@ -6,7 +6,7 @@ import itertools
 from sklearn.preprocessing import StandardScaler
 
 # Se carga archivo csv con las métricas calculadas para cada prueba.
-datos = pd.read_csv('D:/VariablesVisual.csv')
+datos = pd.read_csv('C:/Users/josan/OneDrive/Documentos/Tesis/Memoria de trabajo/Analisis/analisis-tareas-de-memoria-de-trabajo/VariablesVisualActualizado.csv')
 datos=datos[['nivel','blps','mpdc','apcps','pd1','entropy','TTP','PST']]
 datos=datos.astype(float)
 
@@ -27,11 +27,24 @@ seis = []
 macro = []
 weighted = []
 subconjunto=[]
+recallUno=[]
+recallTres=[]
+recallSeis=[]
+recallMacro=[]
+recallWeighted=[]
+f1Uno=[]
+f1Tres=[]
+f1Seis=[]
+f1Macro=[]
+f1Weighted=[]
 count = 1
 for features in all_combinations:
     features = list(features)
     X=datos[features]
-
+    #print('Cantidad de pruebas por nivel:')
+    #print(datos['nivel'].value_counts())
+    #print(X.columns)
+    
     # Se separan los datos para entrenamiento (80%) y evaluación (20%).
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         test_size=0.2,
@@ -46,10 +59,12 @@ for features in all_combinations:
     C = 1.0
     models = svm.SVC(kernel='linear', C=C)
     
+    #print(models)
     models.fit(X_train,y_train)
     y_pred = models.predict(X_test)
     # reporte de estadísticas.
     reporte = classification_report(y_test,y_pred)
+    print(reporte)
      
     uno.append(reporte[74:78])
     tres.append(reporte[128:132])
@@ -57,7 +72,18 @@ for features in all_combinations:
     macro.append(reporte[291:295])
     weighted.append(reporte[345:349])
     subconjunto.append('/'.join(features))
+    recallUno.append(reporte[84:88])
+    recallTres.append(reporte[138:142])
+    recallSeis.append(reporte[192:196])
+    recallMacro.append(reporte[301:305])
+    recallWeighted.append(reporte[355:359])
+    f1Uno.append(reporte[94:98])
+    f1Tres.append(reporte[148:152])
+    f1Seis.append(reporte[202:206])
+    f1Macro.append(reporte[311:315])
+    f1Weighted.append(reporte[365:369])
     print(count)
     count += 1
-reporte = pd.DataFrame({'subconjunto':subconjunto,'uno':uno,'tres':tres,'seis':seis,'macro':macro,'weighted':weighted})
-reporte.to_excel('linear.xlsx')
+    #print("********************")
+reporte = pd.DataFrame({'Subset':subconjunto,'Precision:L':uno,'Precision:M':tres,'Precision:H':seis,'Precision:Macro':macro,'Precision:Weighted':weighted,'Recall:L':recallUno,'Recall:M':recallTres,'Recall:H':recallSeis,'Recall:Macro':recallMacro,'Recall:Weighted':recallWeighted,'F1:L':f1Uno,'F1:M':f1Tres,'F1:H':f1Seis,'F1:Macro':f1Macro,'F1:Weighted':f1Weighted})
+reporte.to_excel('linear2.xlsx')
